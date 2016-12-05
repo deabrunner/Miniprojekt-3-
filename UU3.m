6ad60958327b6eba5bdd18c450c32a098bf16475
Dea Brunner <deabrunner@gmail.com>
2016-12-05 15:56:18 +0100

    Senaste

--- UU3.m
+++ UU3.m
@@ -1,31 +1,36 @@
 clear all
 close all
 
-N=100
-svektor=(0:N)/N;
+N=40;
+ds=1/N;
+s=(0:N)*ds;
 A=5; 
 k=3;
 t=0;
-stegl=1/100
-svektor=(0:N)/N
-thv_ana = (A/(k*pi))*((sin(k*pi*svektor + 2*pi*t)) - sin(2*pi*t))
-ds=1/N;
-thv_num=stegl*cumtrapz(svektor)
-kappav=A*cos(k*pi*svektor+2*pi*t)
-thv_num0=zeros(size(svektor));
-thv_numM=ds*cumtrapz(kappav)
+
+thv_ana = (A/(k*pi))*((sin(k*pi*s + 2*pi*t)) - sin(2*pi*t));
+kappav=A*cos(k*pi*s+2*pi*t);
+thv_numM=ds*cumtrapz(kappav);
+thv_num=zeros(size(s));
+
 
 for i=1:N
-    
-    thv_num(i+1)=thv_num(i)+(stegl/2)*((kappav(i)+kappav(i+1)))
-    diff=thv_ana-thv_num;
-    subplot(3,1,1)
-    plot(svektor, diff)
-    hold on;
-    subplot(3,1,2)
-    plot(svektor,thv_num)
-    hold on;
+    thv_num(i+1)=thv_num(i)+(ds/2)*((kappav(i)+kappav(i+1)));
 end
-subplot(3,1,3)
-plot(svektor,thv_numM)
+    diff1=thv_ana-thv_num;
+    diff2=thv_numM-thv_num;
+    subplot(4,1,1)
+    plot(s, diff1)
+    hold on;
+    subplot(4,1,2)
+    plot(s,thv_num)
+    hold on;
+    
+subplot(4,1,3)
+plot(s,thv_ana)
+subplot(4,1,4)
+plot(s,thv_numM)
+
+fel1 = sqrt(sum(diff1.^2)/numel(diff1))
+fel2 = sqrt(sum(diff2.^2)/numel(diff2))
 
