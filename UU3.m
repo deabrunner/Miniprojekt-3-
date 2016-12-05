cc390202ab66564fa8b16f85acaea2fbf331d6d5
Dea Brunner <deabrunner@gmail.com>
2016-12-05 13:37:09 +0100

    Ändring

--- UU3.m
+++ UU3.m
@@ -9,20 +9,23 @@ t=0;
 stegl=1/100
 svektor=(0:N)/N
 thv_ana = (A/(k*pi))*((sin(k*pi*svektor + 2*pi*t)) - sin(2*pi*t))
+ds=1/N;
 thv_num=stegl*cumtrapz(svektor)
 kappav=A*cos(k*pi*svektor+2*pi*t)
 thv_num0=zeros(size(svektor));
-thv_num=stegl*cumtrapz(svektor)
+thv_numM=ds*cumtrapz(kappav)
 
 for i=1:N
+    
     thv_num(i+1)=thv_num(i)+(stegl/2)*((kappav(i)+kappav(i+1)))
+    diff=thv_ana-thv_num;
+    subplot(3,1,1)
+    plot(svektor, diff)
+    hold on;
+    subplot(3,1,2)
+    plot(svektor,thv_num)
+    hold on;
 end
-thv_ana=(A/(k*pi))*((sin(k*pi*svektor+2*pi*t))-sin(2*pi*t))
-
-diff=thv_ana-thv_num
-
-plot(svektor, diff)
-hold on;
-
-
+subplot(3,1,3)
+plot(svektor,thv_numM)
 
